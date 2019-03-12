889a088215ebf0b0436dd0301a5078bd0f5439fd
Олександр Ступін <o.stupin@ms.npu.edu.ua>
2019-03-12 16:43:13 +0200

    Лабораторна один, версія один.

---
+++ Stupin41PILab01/Stupin41PILab01/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Stupin41PILab01
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        public MainPage()
+        {
+            InitializeComponent();
+            picker.ItemsSource = new List<string>(itSpesiality);
+        }
+
+        int item; 
+        string[] itSpesiality = new string[] { "Тестувальник", "Мобільний розробник", "верстальшик","фронтенд","бекенд" };
+        string[] requrment = new string[] { "Він займається тестуванням проектів", "він розробляє програми дня мобільних пристроїів",
+             "він займається версткою макетів", "він робить усе для фронт розробки", "він робить усе для бекенд розробки" };
+         
+        private void BtnChange_Clicked(object sender, EventArgs e)
+        {
+            lblChange.Text = requrment[item];
+        }
+
+        private void Picker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            item = picker.SelectedIndex;
+        }
+    }
+}
